6692670e771b60baf5c06dc58b41230484842fec
Jonathan Warde <jonwarde@gmail.com>
2019-08-07 17:36:44 +0100

    Chrome native image lazy loading with fallback

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,18 @@
         modalSurprise.setContent('<img src="img/supplier-showcase.jpg"><p>Description here</p>');
         modalSurprise.open();
     });
+
+
+   if ('loading' in HTMLImageElement.prototype) {
+      const images = document.querySelectorAll("img.lazyload");
+      images.forEach(img => {
+          img.src = img.dataset.src;
+      });
+  } else {
+      // Dynamically import the LazySizes library
+    let script = document.createElement("script");
+    script.async = true;
+    script.src =
+      "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js";
+    document.body.appendChild(script);
+  }
